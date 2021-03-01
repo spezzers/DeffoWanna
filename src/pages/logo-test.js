ae2a51226a3bc4ed5130194d26c9fad1df41c44b
spezzers <54096233+spezzers@users.noreply.github.com>
2021-03-01 22:14:34 +0000

    logo enhancements

--- src/pages/logo-test.js
+++ src/pages/logo-test.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Logo from '../components/Logo'
+import styled from 'styled-components'
+
+const Wrap = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100vw;
+	height: 100vh;
+`
 
 const LogoTest = () => {
-
 	return (
 		<Layout>
-			<Logo />
+			<Wrap>
+				<Logo color='#52a' highlightColor='#cfd' />
+			</Wrap>
 		</Layout>
 	)
 }

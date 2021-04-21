97180be8dcf3bfe81722cd7626a1c2295c2afa03
spezzers <54096233+spezzers@users.noreply.github.com>
2021-04-21 19:37:17 +0100

    Color Story

--- src/stories/Colors.stories.js
+++ src/stories/Colors.stories.js
@@ -1,30 +1,56 @@
 import React from 'react'
 import { light, dark } from '../components/themes'
+import styled from 'styled-components'
 
-const Colors = ({ theme }) => {
-	// const colors = Object.entries(theme)
-	// const colors = Object.entries(theme)
-	// console.log(colors)
+const Color = props => {
 	return (
-		<table>
-			<tbody>
-				{Object.entries(theme).map(entry => {
-					const [key, value] = entry
-					return (
-						<tr>
-							<td>{key}</td>
-							<td>{value}</td>
-						</tr>
-					)
-				})}
-			</tbody>
-		</table>
-		// <div>{colors}</div>
+		<div className='row'>
+			<div className='label'>{props.label}</div>
+			<div style={{ backgroundColor: `${props.value}` }} className='value'>
+				{props.value}
+			</div>
+		</div>
 	)
 }
+const ColorsList = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-content: center;
+	.row {
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		margin: 5px;
+		.value {
+			padding: 15px;
+			flex-grow: 10;
+		}
+		.label {
+			flex-grow: 1;
+			flex-basis: 1;
+		}
+	}
+`
+
+const Colors = ({ theme }) => {
+	return (
+		<ColorsList>
+			{Object.entries(theme).map(entry => {
+				const [key, value] = entry
+				if (key === 'name') {
+					return <h1 key={key}>{value} theme</h1>
+				}
+				return <Color key={key} label={key} value={value} />
+			})}
+		</ColorsList>
+	)
+}
+
 export default {
 	title: 'Design System/Colors',
 	component: Colors
 }
 
-export const Primary = () => <Colors theme={light} />
+export const Light = () => <Colors theme={light} />
+export const Dark = () => <Colors theme={dark} />

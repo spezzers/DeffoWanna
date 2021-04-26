89d7d6146ffaef6dae19985a1cac547d5cef680d
spezzers <54096233+spezzers@users.noreply.github.com>
2021-04-26 08:41:45 +0100

    working on storybook theming

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,22 @@
+import {light, dark} from '../src/components/themes'
+
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  layout: 'centered',
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: light.bg
+      },
+      {
+        name: 'dark',
+        value: dark.bg
+      }
+    ]
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,

e32cb3803a1e4e53fa7dc5cd6aeaea3e9e29ad0a
spezzers <54096233+spezzers@users.noreply.github.com>
2021-06-20 17:45:50 +0100

    given up on mdx docs: code and args preview issues

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,10 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-docs',
+		'@storybook/addon-essentials',
+		'@storybook/addon-storysource',
+		'@storybook/addon-actions'
+	]
 }

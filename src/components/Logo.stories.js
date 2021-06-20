5a0618fa5d843bb68715fe4472adff011edac565
spezzers <54096233+spezzers@users.noreply.github.com>
2021-06-20 15:57:39 +0100

    dependency and  story updates

--- src/components/Logo.stories.js
+++ src/components/Logo.stories.js
@@ -3,11 +3,36 @@ import Logo from './Logo'
 
 export default {
 	title: 'Components/Logo',
-	component: Logo
+	component: Logo,
+	argTypes: {
+		size: {
+			name: 'Size',
+			description: 'Font size of the logo',
+			control: 'number',
+			table: {
+				defaultValue: {
+					summary: 4
+				},
+				type: {
+					summary: 'rem',
+					detail: 'Overall height is 2.2x the rem number'
+				}
+			}
+		},
+		color: {
+			name: 'Color',
+			control: 'color',
+			table: {
+				defaultValue: {
+					summary: '#ff99344'
+				}
+			}
+		}
+	}
 }
 
 const Template = args => <Logo {...args}/>
 
 export const Primary = Template.bind({})
 Primary.storyName = 'Default'
-Primary.args = {rem: 4}
+Primary.args = {size: 4}

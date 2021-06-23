9e57dd0adc1539ea80fe9c0e155ac9c1e4f89c6b
spezzers <54096233+spezzers@users.noreply.github.com>
2021-06-23 15:29:39 +0100

    testing browser support

--- src/components/Logo-README.md
+++ src/components/Logo-README.md
@@ -3,3 +3,6 @@
 So. Thanks to SVG filter effects, I can take a black and white WebM video, *with **no** transparency*, and produce a colourized version **with** transparency. Yay!
 
 This is exciting because it will allow me to use more complex animations in the smaller video file but still adjust the colour dynamically.
+
+## What I've learnt so far
+- WebM video isn't supported on iOS
--- src/components/WebmVideoTest.js
+++ src/components/WebmVideoTest.js
@@ -1,49 +1,58 @@
 import React from 'react'
 import LogoAnimation from '../assets/logoAnimation.webm'
+import LogoAnimationMP4 from '../assets/logoAnimation.mp4'
 
 const SvgVid = () => {
 	return (
-		<svg
-			viewBox='0 0 1080 540'
-			height='8rem'
-			preserveAspectRatio='xMinYMin'
-			xmlns='http://www.w3.org/2000/svg'
-		>
-			<defs>
-				<filter id='vidfilter'>
-					<feColorMatrix
-						in='SourceGraphic'
-						result='alpha'
-						type='matrix'
-						values=' 1  0  0  0  0 
+		<>
+			<svg
+				viewBox='0 0 1080 540'
+				height='8rem'
+				preserveAspectRatio='xMinYMin'
+				xmlns='http://www.w3.org/2000/svg'
+			>
+				<defs>
+					<filter id='vidfilter'>
+						<feColorMatrix
+							in='SourceGraphic'
+							result='alpha'
+							type='matrix'
+							values=' 1  0  0  0  0 
 								0  1  0  0  0 
 								0  0  1  0  0 
 								-10 -10 -10  1  0'
-					/>
-					<feFlood result='fill' floodColor='red' />
+						/>
+						<feFlood result='fill' floodColor='red' />
 
-					<feComposite in2='alpha' in='fill' operator='in' />
-
-				
-				</filter>
-			</defs>
-			<foreignObject
-				id='vid'
-				width='100%'
-				height='100%'
-				filter='url(#vidfilter)'
-			>
+						<feComposite in2='alpha' in='fill' operator='in' />
+					</filter>
+				</defs>
+				<foreignObject
+					id='vid'
+					width='100%'
+					height='100%'
+					filter='url(#vidfilter)'
+				>
 					<style>filter='invert(100%)'</style>
-					<video width='100%' height='100%' autoPlay loop muted>
-						<source src={LogoAnimation} type='video/webm' />
+					<video width='100%' height='100%' autoPlay playsInline loop muted>
+						{/* FIX Dang! WebM not working on iOS */}
+						{/* BUG Firefox only plays video on mouseEvents - very weird! */}
+						<source src={LogoAnimation} type='video/mp4' />
+						<source src={LogoAnimationMP4} type='video/mp4' />
 					</video>
-			</foreignObject>
-		</svg>
+				</foreignObject>
+			</svg>
+			<video playsInline width='100%' height='100%' autoPlay loop muted>
+				{/* NOTE check if ios has issue with file format */}
+				<source src={LogoAnimation} type='video/webm' />
+				<source src={LogoAnimationMP4} type='video/mp4' />
+			</video>
+		</>
 	)
 }
 
 const WebmVideoTest = () => {
 	return <SvgVid />
 }
-
+// TODO rename this component and update references
 export default WebmVideoTest

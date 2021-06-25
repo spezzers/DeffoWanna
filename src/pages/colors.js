fe0e27d3d405da9f5d2394294b40af48f8156b5b
spezzers <54096233+spezzers@users.noreply.github.com>
2021-06-25 17:22:00 +0100

    testing dark mode css filter on images

---
+++ src/pages/colors.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+import colorPalette from '../images/Color Palette.png'
+import sample from '../images/sample.jpg'
+import themes from '../styles/themes'
+
+const Wrap = styled.div`
+	div {
+		padding: 30px;
+		width: 300px;
+		display: inline-block;
+		img {
+			width: 100%;
+		}
+	}
+	.left {
+		padding-right: 0;
+	}
+	.right {
+		padding-left: 0;
+	}
+	.light {
+		background-color: ${themes.light.background};
+	}
+	.dark {
+		background-color: ${themes.dark.background};
+		img {
+			filter: brightness(88%) saturate(95%) contrast(111%);
+		}
+	}
+`
+
+const colors = () => {
+	return (
+		<Wrap>
+			<div className='dark left'>
+				<img src={colorPalette} alt='color palette dark' />
+			</div>
+			<div className='light right'>
+				<img src={colorPalette} alt='color palette light' />
+			</div>
+			<div className='dark'>
+				<img className='dark' src={sample} alt='sample dark' />
+			</div>
+			<div className='light'>
+				<img className='light' src={sample} alt='sample light' />
+			</div>
+		</Wrap>
+	)
+}
+
+export default colors

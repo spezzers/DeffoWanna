46cb4f4eb44c3a63d4e492289691c2c44e396314
Ben Morton-Sperry <54096233+spezzers@users.noreply.github.com>
2021-07-05 17:57:43 +0100

    rotation args

--- src/components/Skateboard.js
+++ src/components/Skateboard.js
@@ -6,20 +6,36 @@ const StyledDeck = styled.div`
 	--width: ${props => props.width || '80px'};
 	--height: ${props => props.height || '320px'};
 	--radius: calc(var(--width) / 2);
+	--rotate: ${props => props.rotateX ? `${props.rotateX}deg` : '0deg'};
 	transform-style: preserve-3d;
-	background-color: ${props => props.color || 'red'};
 	width: var(--width);
 	height: var(--height);
 	border-radius: calc(var(--width) / 2);
-	transform: rotateY(30deg) rotateX(-5deg);
-	.nose, .tail {
+	transform: rotateY(var(--rotate));
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	.mid {
+		flex-grow: 1;
+	}
+	.nose, .tail, .mid {
 		width: var(--width);
-		height: calc(var(--width) * 0.75);
-		border-radius: var(--radius) var(--radius) 0 0;
 		background-color: blue;
 	}
+
+	.nose, .tail {
+		height: calc(var(--width) * 0.75);
+		border-radius: var(--radius) var(--radius) 0 0;
+	}
+	.nose {
+		transform-origin: bottom;
+		transform: rotateX(15deg);
+
+	}
 	.tail {
-		transform: rotate(180deg);
+		transform-origin: top;
+		transform: rotateX(-15deg);
+		border-radius: 0 0 var(--radius) var(--radius);
 	}
 `
 
--- src/components/Skateboard.stories.js
+++ src/components/Skateboard.stories.js
@@ -3,7 +3,28 @@ import Skateboard from './Skateboard'
 
 export default {
 	title: 'Components/Image Frames/Skateboard',
-	component: Skateboard
+	component: Skateboard,
+	argTypes: {
+		rotateX: {
+			name: 'rotate',
+			description: 'Deck flip rotation',
+			control: {
+				type: 'range',
+				min: -180,
+				max: 180,
+				step: 5
+			},
+			table: {
+				defaultValue: {
+					summary: 0
+				},
+				type: {
+					summary: 'deg',
+					detail: 'degrees'
+				}
+			}
+		}
+	}
 }
 
 export const Primary = args => <Skateboard {...args}/>

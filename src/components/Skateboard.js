722a2f787b2fcdce75a53b38538d376ccca3a78b
Ben Morton-Sperry <54096233+spezzers@users.noreply.github.com>
2021-07-05 09:31:40 +0100

    Skateboard setting up

---
+++ src/components/Skateboard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const StyledDeck = styled.div`
+	transform-style: preserve-3d;
+`
+
+const Skateboard = () => {
+	return(
+		<StyledDeck>
+			Skateboard.js
+		</StyledDeck>
+	)
+}
+
+export default Skateboard
---
+++ src/components/Skateboard.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Skateboard from './Skateboard'
+
+export default {
+	title: 'Components/Image Frames/Skateboard',
+	component: Skateboard
+}
+
+export const Primary = args => <Skateboard {...args}/>

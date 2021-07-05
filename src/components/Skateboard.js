8f83d3df822b758f7cfe03e4d41a685ec385ac94
Ben Morton-Sperry <54096233+spezzers@users.noreply.github.com>
2021-07-05 09:58:59 +0100

    deck development

--- src/components/Skateboard.js
+++ src/components/Skateboard.js
@@ -3,13 +3,32 @@ import styled from 'styled-components'
 
 
 const StyledDeck = styled.div`
+	--width: ${props => props.width || '80px'};
+	--height: ${props => props.height || '320px'};
+	--radius: calc(var(--width) / 2);
 	transform-style: preserve-3d;
+	background-color: ${props => props.color || 'red'};
+	width: var(--width);
+	height: var(--height);
+	border-radius: calc(var(--width) / 2);
+	transform: rotateY(30deg) rotateX(-5deg);
+	.nose, .tail {
+		width: var(--width);
+		height: calc(var(--width) * 0.75);
+		border-radius: var(--radius) var(--radius) 0 0;
+		background-color: blue;
+	}
+	.tail {
+		transform: rotate(180deg);
+	}
 `
 
-const Skateboard = () => {
+const Skateboard = (props) => {
 	return(
-		<StyledDeck>
-			Skateboard.js
+		<StyledDeck {...props}>
+			<div className='nose'></div>
+			<div className='mid'></div>
+			<div className='tail'></div>
 		</StyledDeck>
 	)
 }

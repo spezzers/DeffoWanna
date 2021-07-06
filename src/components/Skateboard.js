312492c1e10bfb63a6cf75faf25e1435899bac68
Ben Morton-Sperry <54096233+spezzers@users.noreply.github.com>
2021-07-06 11:56:40 +0100

    flippin' good

--- src/components/Skateboard.js
+++ src/components/Skateboard.js
@@ -1,36 +1,52 @@
 import React from 'react'
 import styled from 'styled-components'
 
-
 const StyledDeck = styled.div`
 	--width: ${props => props.width || '80px'};
 	--height: ${props => props.height || '320px'};
 	--radius: calc(var(--width) / 2);
-	--rotate: ${props => props.rotateX ? `${props.rotateX}deg` : '0deg'};
+	--lat-flip: ${props => (props.latFlip ? `${props.latFlip}deg` : '0deg')};
+	--long-flip: ${props => (props.longFlip ? `${props.longFlip}deg` : '0deg')};
+	--spin: ${props => (props.spin ? `${props.spin}deg` : '0deg')};
+	--rotate-x: ${props => (props.rotateX ? `${props.rotateX}deg` : '0deg')};
+	--rotate-y: ${props => (props.rotateY ? `${props.rotateY}deg` : '0deg')};
 	transform-style: preserve-3d;
-	width: var(--width);
-	height: var(--height);
-	border-radius: calc(var(--width) / 2);
-	transform: rotateY(var(--rotate));
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
+	transform: rotateX(var(--rotate-x)) rotateZ(var(--rotate-y));
+	* {
+		transform-style: preserve-3d;
+	}
+	.deck {
+		width: var(--width);
+		height: var(--height);
+		border-radius: calc(var(--width) / 2);
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+	.flip {
+		transform: rotateX(var(--long-flip)) rotateY(var(--lat-flip)) ;
+	}
+	.spin {
+		transform: rotateZ(var(--spin));
+	}
 	.mid {
 		flex-grow: 1;
 	}
-	.nose, .tail, .mid {
+	.nose,
+	.tail,
+	.mid {
 		width: var(--width);
 		background-color: blue;
 	}
 
-	.nose, .tail {
+	.nose,
+	.tail {
 		height: calc(var(--width) * 0.75);
 		border-radius: var(--radius) var(--radius) 0 0;
 	}
 	.nose {
 		transform-origin: bottom;
 		transform: rotateX(15deg);
-
 	}
 	.tail {
 		transform-origin: top;
@@ -39,12 +55,18 @@ const StyledDeck = styled.div`
 	}
 `
 
-const Skateboard = (props) => {
-	return(
+const Skateboard = props => {
+	return (
 		<StyledDeck {...props}>
-			<div className='nose'></div>
-			<div className='mid'></div>
-			<div className='tail'></div>
+			<div className='spin'>
+				<div className='flip'>
+					<div className='deck'>
+						<div className='nose'></div>
+						<div className='mid'></div>
+						<div className='tail'></div>
+					</div>
+				</div>
+			</div>
 		</StyledDeck>
 	)
 }
--- src/components/Skateboard.stories.js
+++ src/components/Skateboard.stories.js
@@ -6,8 +6,84 @@ export default {
 	component: Skateboard,
 	argTypes: {
 		rotateX: {
-			name: 'rotate',
-			description: 'Deck flip rotation',
+			name: 'rotate-x',
+			description: 'Overall deck rotation on X axis',
+			control: {
+				type: 'range',
+				min: -180,
+				max: 180,
+				step: 5
+			},
+			table: {
+				defaultValue: {
+					summary: 0
+				},
+				type: {
+					summary: 'deg',
+					detail: 'degrees'
+				}
+			}
+		},
+		rotateY: {
+			name: 'rotate-y',
+			description: 'Overall deck rotation on Y axis',
+			control: {
+				type: 'range',
+				min: -180,
+				max: 180,
+				step: 5
+			},
+			table: {
+				defaultValue: {
+					summary: 0
+				},
+				type: {
+					summary: 'deg',
+					detail: 'degrees'
+				}
+			}
+		},
+		latFlip: {
+			name: 'lat-flip',
+			description: 'Deck flip rotation (trick example: kickflip)',
+			control: {
+				type: 'range',
+				min: -180,
+				max: 180,
+				step: 5
+			},
+			table: {
+				defaultValue: {
+					summary: 0
+				},
+				type: {
+					summary: 'deg',
+					detail: 'degrees'
+				}
+			}
+		},
+		longFlip: {
+			name: 'long-flip',
+			description: 'Deck vertical rottion (trick example: impossible)',
+			control: {
+				type: 'range',
+				min: -180,
+				max: 180,
+				step: 5
+			},
+			table: {
+				defaultValue: {
+					summary: 0
+				},
+				type: {
+					summary: 'deg',
+					detail: 'degrees'
+				}
+			}
+		},
+		spin: {
+			name: 'spin',
+			description: 'Deck spin rotation (trick example: shove-it)',
 			control: {
 				type: 'range',
 				min: -180,

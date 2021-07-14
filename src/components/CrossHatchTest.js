46dd44ecf2e3dc9da7a503f15a7acff53298c6f9
spezzers <54096233+spezzers@users.noreply.github.com>
2021-07-14 14:17:56 +0100

    get rid of unecessary background hatching

--- src/components/CrossHatchTest.js
+++ src/components/CrossHatchTest.js
@@ -24,18 +24,19 @@ const HatchedImage = styled.div.attrs(props => {
 		return {
 			blend: 'multiply',
 			themeFilters: 'brightness(1.25)',
-			invert: 'invert(0) contrast(0.5)'
+			invert: 'invert(0) contrast(0.5)',
+			contrast: 'black'
 		}
 	} else {
 		return {
 			blend: 'color-dodge',
 			themeFilters: 'brightness(0.9)',
-			invert: 'invert(1) contrast(0.5)'
+			invert: 'invert(1) contrast(0.5)',
+			contrast: 'white'
 		}
 	}
 })`
-	filter: grayscale(1) contrast(500) 
-		${props => props.themeFilters} blur(0.4px) !important;
+	filter: grayscale(1) contrast(500) ${props => props.themeFilters} blur(0.4px) !important;
 	margin: 0;
 	mix-blend-mode: ${props => props.blend};
 	opacity: 0.75;
@@ -46,16 +47,30 @@ const HatchedImage = styled.div.attrs(props => {
 		background-repeat: repeat;
 		background-size: 70px;
 		overflow-y: hidden;
-		
-		svg,
-		img {
-			filter: ${props => props.invert} blur(0);
-			display: block;
-			background-color: white !important;
-			mix-blend-mode: hard-light;
-			box-shadow: 0 0 20px 15px white;
-		}
 
+		.content {
+			background-color: white;
+			mix-blend-mode: hard-light;
+			display: block;
+
+			p,
+			h1,
+			h2,
+			h3,
+			h4,
+			h5,
+			h6 {
+				color: black;
+				:selection {
+					color: black;
+					background: white;
+				}
+			}
+			svg,
+			img {
+				filter: ${props => props.invert} blur(0);
+			}
+		}
 	}
 `
 
@@ -63,11 +78,15 @@ const CrossHatchTest = () => {
 	const theme = useContext(ThemeContext)
 	return (
 		<>
+			<img src={shortHatch} alt='' display='none' width='0' />
 			<HatchedImage theme={theme}>
 				<div className='hatch'>
-					<DemoSVG />
-					<img src={pic} width='600px' alt='birds' />
-					<Logo color='black' />
+					<div className='content'>
+						<DemoSVG />
+						<h1>Graphic Design & Web Development</h1>
+						<img src={pic} width='600px' alt='birds' />
+						<Logo color={theme.name === 'light' ? 'black' : 'white'} />
+					</div>
 				</div>
 			</HatchedImage>
 		</>

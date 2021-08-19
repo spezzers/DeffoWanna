c31458c7bc8136c20cff64eb327d4e723303dcc1
spezzers <54096233+spezzers@users.noreply.github.com>
2021-08-19 19:02:40 +0100

    markdown blog customization

--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -1,32 +1,45 @@
-import React from "react"
-import { graphql } from "gatsby"
-export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark
-  return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
-  )
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import styled from 'styled-components'
+import { Helmet } from 'react-helmet'
+
+const BlogPost = styled.div`
+	grid-area: main;
+`
+
+const Template = ({
+	data // this prop will be injected by the GraphQL query below.
+}) => {
+	const { markdownRemark } = data // data.markdownRemark holds your post data
+	const { frontmatter, html } = markdownRemark
+	console.log(data.markdownRemark)
+	return (
+		<Layout>
+			<Helmet>
+				<title>{frontmatter.title} - Deffo Wanna Blog</title>
+			</Helmet>
+			<BlogPost>
+				<h1>{frontmatter.title}</h1>
+				<h2>{frontmatter.date}</h2>
+				<div
+					className='blog-post-content'
+					dangerouslySetInnerHTML={{ __html: html }}
+				/>
+			</BlogPost>
+		</Layout>
+	)
 }
 export const pageQuery = graphql`
-  query($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      html
-      frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        slug
-        title
-      }
-    }
-  }
+	query($id: String!) {
+		markdownRemark(id: { eq: $id }) {
+			html
+			frontmatter {
+				date(formatString: "MMMM DD, YYYY")
+				slug
+				title
+			}
+		}
+	}
 `
+export default Template

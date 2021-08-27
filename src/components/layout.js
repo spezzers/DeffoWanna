3e7b4b4fff76ebdb53b36d5dd8c6875e6aeabc74
spezzers <54096233+spezzers@users.noreply.github.com>
2021-08-27 07:41:19 +0100

    added ratio const for line height

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,10 +6,11 @@ import '../styles/layout.css'
 import { Link } from 'gatsby'
 
 const fontSizePx = 19
+const lineHeightRatio = 1.263
 export const fontSize = `${fontSizePx}px`
-export const lineHeight = `${fontSizePx * 1.263}px`
+export const lineHeight = `${fontSizePx * lineHeightRatio}px`
 export const rowGap = lineHeight
-export const colGap = `${((fontSizePx * 1.263) / 5) * 8}px`
+export const colGap = `${((fontSizePx * lineHeightRatio) / 5) * 8}px`
 export const breakpoint = {
 	mobile: '@media only screen and (max-width: 31rem)',
 	tablet: '@media only screen and (min-width: 31rem)',

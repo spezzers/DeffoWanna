e55e4b1e8cfdb1713d189c124d5e83f6bf79fbcd
spezzers <54096233+spezzers@users.noreply.github.com>
2021-09-12 20:25:53 +0100

    separation of footnotes from markdown pages

--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -3,15 +3,24 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import styled from 'styled-components'
 import { Helmet } from 'react-helmet'
-import {GatsbyImage, getImage} from 'gatsby-plugin-image'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { lineHeight, smallRow } from '../styles/sizes'
+import { themeContextColor } from '../styles/themes'
 
-const BlogPost = styled.div`
-		grid-area: main;
-		grid-row: 2;
-		img {
-			max-width: 100%;
-		}
+const BlogPost = styled.article`
+	grid-column: main-start / main-end;
+	grid-row: ${props => console.log(props.rowStart)};
+	img {
+		max-width: 100%;
+	}
+`
+const Footnotes = styled.section`
+	grid-row: ${props => props.rowStart};
+	grid-column: main-start / main-end;
+	font-weight: 200;
+	font-size: 1rem;
+	color: ${themeContextColor('textStrong')};
+	border-top: 1px solid ${themeContextColor('text')}
 `
 
 const HeroImg = styled(GatsbyImage)`
@@ -24,29 +33,59 @@ const HeroImg = styled(GatsbyImage)`
 	margin-top: -${lineHeight};
 `
 
-const Template = ({
-	data // this prop will be injected by the GraphQL query below.
-}) => {
-	const { markdownRemark } = data // data.markdownRemark holds your post data
+const Template = ({ data }) => {
+	const { markdownRemark } = data
 	const { frontmatter, html } = markdownRemark
 	const heroImage = getImage(frontmatter.hero?.childImageSharp?.gatsbyImageData)
 	const hero = heroImage ? (
-		<HeroImg image={heroImage} alt={frontmatter.alt || `hero image: ${frontmatter.title}`}/>
+		<HeroImg
+			image={heroImage}
+			alt={frontmatter.alt || `hero image: ${frontmatter.title}`}
+		/>
 	) : null
+
+	if (html.includes('<div class="footnotes">')) {
+		const splitHtml = html.split('<div class="footnotes">')
+		let splitFootnotes = splitHtml[1].split('\n')
+		const footnotes = splitFootnotes.slice(2, -1).join('')
+		splitFootnotes.pop()
+		return (
+			<Layout>
+				<Helmet>
+					<title>{frontmatter.title} - Deffo Wanna Blog</title>
+				</Helmet>
+				{hero}
+				<BlogPost
+					rowStart={hero ? 2 : 1}
+					className='blog-post-content'
+					dangerouslySetInnerHTML={{
+						__html: `<h1>${frontmatter.title}</h1><h2>${frontmatter.date}</h2>${splitHtml[0]}`
+					}}
+				/>
+				<Footnotes
+					rowStart={hero ? 3 : 2}
+					className='footnotes'
+					dangerouslySetInnerHTML={{
+						__html: `<h6>Footnotes</h6>${footnotes}`
+					}}
+				/>
+			</Layout>
+		)
+	}
+
 	return (
 		<Layout>
 			<Helmet>
 				<title>{frontmatter.title} - Deffo Wanna Blog</title>
 			</Helmet>
 			{hero}
-			<BlogPost>
-				<h1>{frontmatter.title}</h1>
-				<h2>{frontmatter.date}</h2>
-				<div
-					className='blog-post-content'
-					dangerouslySetInnerHTML={{ __html: html }}
-				/>
-			</BlogPost>
+			<BlogPost
+				rowStart={hero ? 2 : 1}
+				className='blog-post-content'
+				dangerouslySetInnerHTML={{
+					__html: `<h1>${frontmatter.title}</h1><h2>${frontmatter.date}</h2>${html}`
+				}}
+			/>
 		</Layout>
 	)
 }
@@ -54,13 +93,16 @@ export const pageQuery = graphql`
 	query($id: String!) {
 		markdownRemark(id: { eq: $id }) {
 			html
+			htmlAst
 			frontmatter {
 				date(formatString: "MMMM DD, YYYY")
 				slug
 				title
 				alt
 				hero {
-					childImageSharp { gatsbyImageData }
+					childImageSharp {
+						gatsbyImageData
+					}
 				}
 			}
 		}

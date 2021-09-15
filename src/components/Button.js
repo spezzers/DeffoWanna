e8ba44921d5195b55f10492de4abd4de9e35bbc7
spezzers <54096233+spezzers@users.noreply.github.com>
2021-09-15 17:30:49 +0100

    a bit better button - customizable colors

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,21 +3,37 @@ import styled from 'styled-components'
 import { colGap, lineHeight } from '../styles/sizes'
 import { themeContextColor } from '../styles/themes'
 
-const _Button = styled.button.attrs(props => {
-	const newProps = props.primary
-		? { main: 'purpleText',
-			bg: 'background',
-			black: 'black',
-			white: 'white',
-			contrast: 'contrast'
-		}
-		: {
-			main: 'background',
-			bg: 'purpleTextStrong',
-			black: 'purpleBg',
-			white: 'purpleText',
-			contrast: 'purpleText'
-		}
+const StyledButton = styled.button.attrs(props => {
+	const color = (themeColor, append) => {
+		const validColors = ['purple', 'blue', 'teal', 'green', 'yellow', 'orange', 'red']
+		const validAppends = ['Bg', 'Text', 'TextStrong']
+		if (
+			validColors.includes(themeColor) &&
+			validAppends.includes(append)
+		) {
+			return `${themeColor}${append}`
+		} 
+		return `purple${append}`
+	}
+
+	const variant = props.variant === 'secondary' ? 'secondary' : 'primary'
+
+	const newProps =
+		variant === 'primary'
+			? {
+					main: color(props.color, 'Text'),
+					bg: props.bgColor ? color(props.bgColor, 'Bg') : 'background',
+					black: 'black',
+					white: 'white',
+					contrast: 'contrast'
+			  }
+			: {
+					main: props.bgColor ? color(props.bgColor, 'Bg') : 'background',
+					bg: color(props.color, 'Text'),
+					black: color(props.color, 'Bg'),
+					white: color(props.color, 'Text'),
+					contrast: color(props.color, 'Text')
+			  }
 
 	return newProps
 })`
@@ -28,42 +44,39 @@ const _Button = styled.button.attrs(props => {
 	--white: ${props => themeContextColor(props.white)};
 	background-color: var(--main);
 	margin: calc(${lineHeight} / 2) calc(${colGap} / 2);
-	font-size: 1em;
+	font-size: .9rem;
 	font-weight: 600;
 	font-family: IBM Plex Sans, sans-serif;
 	color: var(--bg);
-	border-radius: .5em;
-	transform: translateY(-.125em);
+	border-radius: 0.5em;
+	transform: translateY(-0.125em);
 	filter: none;
-	border: .1em solid var(--main);
+	border: 0.1em solid var(--main);
 	text-align: center;
 	line-height: ${lineHeight};
 	padding: calc(${lineHeight} / 2) calc(${colGap} / 2);
-	box-shadow:
-		0 0 0 0.1em var(--white),
-		0 0.2em 0 0.2em var(--black),
+	box-shadow: 0 0 0 0.1em var(--white), 0 0.2em 0 0.2em var(--black),
 		0 0.2em 0 0.4em var(--contrast);
 	cursor: pointer;
-	transition: all .02s ease-in-out;
-	:hover, :focus {
+	transition: all 0.02s ease-in-out;
+	:hover,
+	:focus {
 		filter: contrast(2);
 		outline: none;
 	}
 	:active {
 		filter: saturate(3);
 		transform: translateY(0.075em);
-		box-shadow:
-		0 0 0 0.1em var(--white),
-		0 0.025em 0 0.2em var(--black),
-		0 0.025em 0 0.4em var(--contrast);
+		box-shadow: 0 0 0 0.1em var(--white), 0 0.025em 0 0.2em var(--black),
+			0 0.025em 0 0.4em var(--contrast);
 	}
 `
 
-const Button = (props) => {
-	return(
-		<_Button {...props} type='button'>
+const Button = props => {
+	return (
+		<StyledButton {...props} type='button' label='Dude'>
 			{props.children}
-		</_Button>
+		</StyledButton>
 	)
 }
 
--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -1,27 +1,76 @@
 import React from 'react'
 import Button from './Button'
 
+const colors = ['purple', 'blue', 'teal', 'green', 'yellow', 'orange', 'red']
+
 export default {
-	title: 'Components/UI/Button',
+	title: 'Components/UI/Buttons',
 	component: Button,
 	argTypes: {
-		primary: {
-			name: 'primary',
-			control: 'boolean'
+		variant: {
+			options: ['primary', 'secondary'],
+			control: { type: 'radio' },
+			description: 'Vary the visual prominence of the button',
+			table: {
+				type: {
+					summary: 'string',
+					detail: 'primary | secondary'
+				},
+				defaultValue: {
+					summary: 'primary',
+				}
+			}
+		},
+		color: {
+			name: 'color',
+			description: 'Main color',
+			options: colors,
+			control: { type: 'select' },
+			table: {
+				type: {
+					summary: 'string',
+					detail: 'name of a color from the app theme'
+				},
+				defaultValue: {
+					summary: 'purple',
+					detail: "theme.purple"
+				}
+			}
+		},
+		bgColor: {
+			name: 'bgColor',
+			description: 'Background color',
+			options: colors,
+			control: {
+				type: 'select'
+			},
+			table: {
+				type: {
+					summary: 'string',
+					detail: 'name of a color from the app theme'
+				},
+				defaultValue: {
+					summary: 'background',
+					detail: "theme.background"
+				}
+			}
+		},
+		children: {
+			description: 'Content that appears on the button',
+			table: {
+				type: {
+					summary: 'text | html',
+				}
+			}
 		}
 	}
 }
 
-const Template = (args, {argTypes}) => (
-	<Button {...args} >{args.children}</Button>
+const Template = (args, { argTypes }) => (
+	<Button {...args}>{args.children}</Button>
 )
 
-export const Primary = Template.bind({})
-Primary.args = {
-	primary: true,
+export const Big = Template.bind({})
+Big.args = {
 	children: 'I like big buttons...'
-}
-export const Secondary = Template.bind({})
-Secondary.args = {
-	children: '...I cannot lie.'
 }

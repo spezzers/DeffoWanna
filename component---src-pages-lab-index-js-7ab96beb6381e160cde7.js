2ffb89e64a457d82cb525aca776e6338c2cb57e2
spezzers <54096233+spezzers@users.noreply.github.com>
2021-10-24 14:15:49 +0100

    Updates

---
+++ component---src-pages-lab-index-js-7ab96beb6381e160cde7.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdeffowanna=self.webpackChunkdeffowanna||[]).push([[828],{5652:function(e,n,a){a.r(n);var t=a(7294),u=a(8639);n.default=function(){return t.createElement(u.Z,null,t.createElement(u.$,null,"Lab: Web Experiments"))}}}]);
+//# sourceMappingURL=component---src-pages-lab-index-js-7ab96beb6381e160cde7.js.map

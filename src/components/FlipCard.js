084d72b74aa6ebb36e0a1f44c959a134c19f6535
spezzers <54096233+spezzers@users.noreply.github.com>
2022-03-28 16:39:13 +0100

    card tilt faces cursor

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -63,7 +63,7 @@ const FlipCard = props => {
 		flipped = flipped !== 0 ? 0 : -180
 		api.start({
 			rotateY: x * maxRotation + flipped,
-			rotateX: y * maxRotation * (flipped ? -1 : 1)
+			rotateX: y * maxRotation * (!flipped ? -1 : 1)
 		})
 	}
 
@@ -82,7 +82,7 @@ const FlipCard = props => {
 
 		api.start({
 			rotateY: x * maxRotation + flipped,
-			rotateX: y * maxRotation * (flipped ? -1 : 1),
+			rotateX: y * maxRotation * (!flipped ? -1 : 1),
 			scale: 1.07,
 			filter: `brightness(${
 				brighten.active + clamp((y - x) / 2, -1, 1) * brighten.tilt
